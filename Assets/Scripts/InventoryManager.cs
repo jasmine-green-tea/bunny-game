bd626c63d2388c7a9a342076821cc419f790749f
eqnØx <76487961+eqn0x@users.noreply.github.com>
2025-10-05 22:39:47 +0300

    F: correct sprite filtration

--- Assets/Scripts/InventoryManager.cs
+++ Assets/Scripts/InventoryManager.cs
@@ -1,8 +1,6 @@
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
-using static UnityEditor.Progress;
-using static UnityEngine.EventSystems.EventTrigger;
 
 public class InventoryManager : MonoBehaviour
 {
